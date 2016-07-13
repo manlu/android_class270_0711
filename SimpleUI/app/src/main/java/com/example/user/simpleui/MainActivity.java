bf214cff64b6a2b8d59329ff0dc06a3fb2105c73
manlu <tseng.manlu@gmail.com>
2016-07-13 10:44:18 +0800

    add listview and spinner

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -4,9 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     EditText editText;
     RadioGroup radioGroup;
+    ListView listView;
+    Spinner spinner;
 
     String selectedTea = "black tea";
 
@@ -25,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView)findViewById(R.id.textView);
         editText = (EditText)findViewById(R.id.editText);//()強制轉型
         radioGroup = (RadioGroup)findViewById(R.id.radiogroup);
+        listView = (ListView)findViewById(R.id.listView);
+        spinner = (Spinner)findViewById(R.id.spinner);
 
         editText.setOnKeyListener(new View.OnKeyListener() {//當使用者按下enter監聽
             @Override
@@ -46,6 +53,26 @@ public class MainActivity extends AppCompatActivity {
                 selectedTea = radioButton.getText().toString();
             }
         });
+
+        setupListView();
+        setupSpinner();
+    }
+
+    public void setupListView()
+    {
+        //item直接寫在程式碼中
+        String[] data = new String[]{"black tea","green tea","1","2","3","4","5"};
+        //轉換器
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
+        listView.setAdapter(adapter);
+    }
+
+    public void setupSpinner()
+    {
+        //讀取寫在array.xml檔中的item
+        String[] data  = getResources().getStringArray(R.array.storeInfos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,data);
+        spinner.setAdapter(adapter);
     }
 
     public void submit(View view)
